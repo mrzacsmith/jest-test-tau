00d303b3a8fe601d560d5f959b9575bb425625f6
Zac Smith <zrs3141592@gmail.com>
2020-02-17 19:51:16 -0700

    create tests for edge cases
    This test included multiplication of 0 and negative numbers"

---
+++ multiply.test.js
@@ -0,0 +1,26 @@
+const multiply = require("./multiply");
+
+describe("testing positive numbers", () => {
+  test("3*2 toBe 6", () => {
+    expect(multiply(3, 2)).toBe(6);
+  });
+  test("3*4 toBe 12", () => {
+    expect(multiply(3, 4)).toBe(12);
+  });
+  test("3*0 toBe 0", () => {
+    expect(multiply(3, 0)).toBe(0);
+  });
+  test("0*5 toBe 0", () => {
+    expect(multiply(0, 5)).toBe(0);
+  });
+  test("-3*4 toBe -12", () => {
+    expect(multiply(-3, 4)).toBe(-12);
+  });
+  test("5*-3 toBe -15", () => {
+    expect(multiply(5, -3)).toBe(-15);
+  });
+});
+
+// toBe tests for values and is strict equality
+
+// toEqual tests value of an object
