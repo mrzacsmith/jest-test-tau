0e261d7db87049d9f389b0491738a1b094538387
Zac Smith <zrs3141592@gmail.com>
2020-02-17 22:13:28 -0700

    test number matches

--- matchesExpanded.test.js
+++ matchesExpanded.test.js
@@ -1,5 +1,18 @@
 test("there is pool in liverpool", () => {
   expect("Liverpool").toMatch(/pool/); // Pass
-  expect("Liverpool").toMatch(/pol/); //fail
+  // expect("Liverpool").toMatch(/pol/); //fail
   expect("Liverpool").not.toMatch(/pol/); //pass
+});
+
+const multiply = require("./multiply.js");
+
+// number matches
+
+describe("test multiply positive senarios", () => {
+  test("multiply 3*2 should equal 6", () => {
+    expect(multiply(3, 2)).toBe(6);
+    expect(multiply(3, 2)).toBeGreaterThan(5);
+    expect(multiply(3, 2)).toBeLessThan(7);
+    expect(multiply(3, 2)).toBeLessThanOrEqual(6);
+  });
 });
