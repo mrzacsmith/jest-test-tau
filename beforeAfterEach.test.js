c3afe4068467a0f277be9ce7db3fd4daf42bf8eb
Zac Smith <zrs3141592@gmail.com>
2020-02-17 22:33:30 -0700

    test using afterEach

--- beforeAfterEach.test.js
+++ beforeAfterEach.test.js
@@ -1,9 +1,13 @@
 const setupFirst = () => {
   console.log("Setting up before test runs");
 };
+const tearDownNow = () => {
+  console.log("Finish up after tests");
+};
 
 describe("new account creation checks", () => {
   beforeEach(() => setupFirst());
+  afterEach(() => tearDownNow());
   test("new account 1 created", () => {
     const account = "account1";
     expect(account).toEqual("account1");
